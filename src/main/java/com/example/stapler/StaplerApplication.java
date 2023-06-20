86d58634be07da5aa7ac1183e123978ce7bf80a6
ElTosheva <elisaveta.tosheva@gmail.com>
2023-06-20 16:56:30 +0300

    Add getters and setters for parent class

--- src/main/java/com/example/stapler/StaplerApplication.java
+++ src/main/java/com/example/stapler/StaplerApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StaplerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StaplerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StaplerApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/stapler/model/OfficeMachine.java
+++ src/main/java/com/example/stapler/model/OfficeMachine.java
@@ -1,7 +1,12 @@
 package com.example.stapler.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Setter
+@Getter
 abstract class OfficeMachine {
     private int id;
     LocalDate createDate;
