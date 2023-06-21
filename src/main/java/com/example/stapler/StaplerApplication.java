19d0a35b411680053fbe22cbf9cf76c4adf42cb5
ElTosheva <elisaveta.tosheva@gmail.com>
2023-06-21 10:16:32 +0300

    Formating

--- src/main/java/com/example/stapler/StaplerApplication.java
+++ src/main/java/com/example/stapler/StaplerApplication.java
@@ -1,6 +1,5 @@
 package com.example.stapler;
 
-import com.example.stapler.model.Stapler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
